f969a06ea8e777e0bda70ddb42df6276f80c9269
Saurabh Sharma <saurabhshcs@gmail.com>
2021-01-20 19:54:06 +0000

    Code commited for initial project setup.

---
+++ src/test/java/com/techsahrezone/springdata/spring/data/example/SpringDataExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techsahrezone.springdata.spring.data.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
