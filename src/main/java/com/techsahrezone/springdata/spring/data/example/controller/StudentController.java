23a98fcde0d2b0fcd952ea73bb847da1e170524a
Saurabh Sharma <saurabhshcs@gmail.com>
2021-01-21 01:06:22 +0000

    [spring-boot-data]- saurabhshcs - Committed initial classes and repository.

---
+++ src/main/java/com/techsahrezone/springdata/spring/data/example/controller/StudentController.java
@@ -0,0 +1,10 @@
+package com.techsahrezone.springdata.spring.data.example.controller;
+
+/*
+ * @created 21/01/2021 -01:04
+ * @project spring-data-example
+ * @author  saurabhshcs
+ */
+
+public class StudentController {
+}
---
+++ src/main/java/com/techsahrezone/springdata/spring/data/example/exception/StudentNotFoundException.java
@@ -0,0 +1,10 @@
+package com.techsahrezone.springdata.spring.data.example.exception;
+
+/*
+ * @created 21/01/2021 -01:05
+ * @project spring-data-example
+ * @author  saurabhshcs
+ */
+
+public class StudentNotFoundException extends RuntimeException {
+}
